727a86ec77bc3f9fbebad3245a80203ec834e37b
ViyanneyDev <viyanneym@crownims.com>
2025-06-05 16:33:19 +0530

    Updated UI of Kotak (05-06-2025)
    Updated UI of Kotak (05-06-2025)

---
+++ src/app/Helper/globalconstants.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+@Injectable()
+export class Globalconstants {
+  readonly baseAPIUrl:
+    //string = 'http://18.223.99.95/GDMS/api/';
+    string = 'http://localhost:50831/api/';
+
+    // string = 'https://demodart.crownims.com/KotakAPI/api/';   //Internal Test
+    // string = 'https://DMSTest.crownims.com/KotakAPI/api/';   //Customer Testing
+
+  //http://localhost:50831/
+  //build//////////////////
+
+  //string = 'http://localhost:50831/api/';
+  //string = 'http://localhost:3626/api/';
+  //string = 'https://storage.janabank.com/JBRDMS/api/';
+  //string = 'https://DMSTest.crownims.com/JBR/api/';
+  //string = 'https://DMSTest.crownims.com/MestoApi/api/';
+}
